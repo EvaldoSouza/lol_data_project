df05ef4e57e87acd3fcedc56f4dd37909042bf62
Evaldo <evaldo.souza2@gmail.com>
2023-04-28 17:07:23 -0300

    Separando as funções complexas da main

--- composite_functions.py
+++ composite_functions.py
@@ -15,27 +15,38 @@ import player_data_collection as pdc
 #         print("Pegando partidas de: ", player)
 #         pdc.get_list_of_matchs_based_on_time(player, primeiro_de_fevereiro_2023)
 
+def adicionar_ID_partidas_em_csv_para_time(nome_do_time, jogadores, data_folder, dia_inicial_em_epoch):
+    path = os.getcwd()
+    data_folder_path = os.path.join(path, data_folder)
+
+    time_pasta = os.path.join(os.path.abspath(data_folder_path), nome_do_time)
+    
+    if not os.path.exists(time_pasta):
+        os.makedirs(time_pasta)
+    
+    os.chdir(time_pasta)
+
+    for player in jogadores:
+        print("Pegando partidas de: ", player)
+        pdc.get_list_of_matchs_based_on_time(player, dia_inicial_em_epoch)
 #Essa função recebe como paramentros um dicionário contendo um dicionário com os times e a lista de players, e uma data inicial
 #a partir da qual pegar os matches de cada jogador de cada time. Essa data tem que ser em segundos Epoch
 #A função irá pegar o nome do player, criar um arquivo com o id de todas as partidas que o player jogou até esse dia inicial
+
 #E então irá consultar a API da Riot para pegar todos os dados dessa partida, salvando em uma pasta com o nome do jogador
-def atualizar_partidas_de_todos_jogadores(times, data_folder, dia_inicial_em_epoch):
-    for time in times.values():
-        for player in time:
-            print("Pegando partidas de: ", player)
-            pdc.get_list_of_matchs_based_on_time(player, dia_inicial_em_epoch)
+def atualizar_partidas_de_todos_jogadores(times, data_folder):
 
     #data_folder = "dados_dos_times_corujão" #nome da pasta para armazenar
     path = os.getcwd()
     data_folder_path = os.path.join(path, data_folder)
 
     for time in times:
-        time_folder = os.path.join(os.path.abspath(data_folder_path), time)
+        time_pasta = os.path.join(os.path.abspath(data_folder_path), time)
         
-        if not os.path.exists(time_folder):
-            os.makedirs(time_folder)
+        if not os.path.exists(time_pasta):
+            os.makedirs(time_pasta)
         
-        os.chdir(time_folder)
+        os.chdir(time_pasta)
         for player in times[time]:
         #print(os.getcwd())
         #print(os.listdir(os.getcwd()))
@@ -47,7 +58,7 @@ def atualizar_partidas_de_todos_jogadores(times, data_folder, dia_inicial_em_epo
                     contents = [ partida.strip() for partida in file]
                     #print( contents)
                     for partida in contents:
-                        if not os.path.exists(partida):
+                        if not os.path.isfile(os.path.join(time_pasta,player, partida)):
                             print("Salvando partida {} de {}".format(partida, player))
                             gmd.store_match_data(gmd.get_all_match_data(partida), player)
                         else:
@@ -65,9 +76,14 @@ def atualizar_partidas_de_todos_jogadores(times, data_folder, dia_inicial_em_epo
 #função para pegar os campeões mais jogados de um conjunto de partidas armazenados em csv em uma pasta
 #pelo padrão dessa aplicação, player_name é também o nome da pasta
 #retorna apenas os campeões mais relevantes, definidos pela medida de relevância
-def search_for_champs_played_in_folder_of_matches(player_name):
+#data_folder é o path completo da pasta em que os dados estão, menos a pasta com o nome do jogador
+def search_for_champs_played_in_folder_of_matches(player_name, player_uuid, data_folder):
+    #mudar o diretoria que a função executa para data_folder
+    os.chdir(data_folder)
     # create a list of file paths matching the pattern '*.csv' in the folder
     file_paths = glob.glob(player_name + '/*.json')#pegar um caminho melhor
+    #print(os.getcwd())
+    #print(os.listdir(os.getcwd()))
 
     #ler o campeão que o player jogou
     #deve conter o campeão como chave,numero de matches jogados, quantidade de vitórias, media de gold por minuto,
@@ -84,53 +100,54 @@ def search_for_champs_played_in_folder_of_matches(player_name):
         
         #preciso checar se o game foi remake
         if game["info"]["gameDuration"] >= tempo_minimo_de_jogo:
-            
-            for participant in game['info']["participants"]:
-                if participant["summonerName"] == player_name:
-                    #dois casos, se o champion já foi lido, ou se não foi
-                    if participant["championName"] in champions_played:
-                        if participant["win"]:
-                            champions_played[participant["championName"]]["Partidas"] += 1
-                            champions_played[participant["championName"]]["Vitorias"] += 1
-                        else:
-                            champions_played[participant["championName"]]["Partidas"] += 1
+            if game["info"]["gameMode"] == "CLASSIC": #Modos não classicos não tem challanges
+                for participant in game['info']["participants"]:
+                    if participant["summonerName"] == player_name or participant["puuid"] == player_uuid: #se o player tiver mudado de nome, buscar por puuid
+                       
+                        #dois casos, se o champion já foi lido, ou se não foi
+                        if participant["championName"] in champions_played:
+                            if participant["win"]:
+                                champions_played[participant["championName"]]["Partidas"] += 1
+                                champions_played[participant["championName"]]["Vitorias"] += 1
+                            else:
+                                champions_played[participant["championName"]]["Partidas"] += 1
+                            
+                            #somando a um valor já existente. Se a chave não existir, será que considera 0 pelo +=?
+                            champions_played[participant["championName"]]["Gold por Minuto"] += participant["challenges"]["goldPerMinute"]
+                            #dano por minuto
+                            champions_played[participant["championName"]]["Dano por Minuto"] += participant["challenges"]["damagePerMinute"]
+                            #visionScore
+                            champions_played[participant["championName"]]["Placar de Visão"] += participant["visionScore"]
+                            #visionScoreAdvantageLaneOpponent //Algumas partidas mais antigas não tem esse dado!
+                            #champions_played[participant["championName"]]["Vantagem de Visão"] += participant["challenges"]["visionScoreAdvantageLaneOpponent"]
+                            #teamDamagePercentage
+                            champions_played[participant["championName"]]["Porcentagem de Dano do Time"] += participant["challenges"]["teamDamagePercentage"]
+                            #damageTakenOnTeamPercentage
+                            champions_played[participant["championName"]]["Porcentagem de Tankada do time"] += participant["challenges"]["damageTakenOnTeamPercentage"]
+                            #damageDealtToObjectives Dar um jeito de fazer isso em porcentagem do time também, para manter a comparação
+                            champions_played[participant["championName"]]["Dano a Objetivos"] += participant["damageDealtToObjectives"]
+                        else:                    
+                            if participant["win"]:
+                                champions_played[participant["championName"]] = {"Partidas": 1, "Vitorias": 1}
+                            else:
+                                champions_played[participant["championName"]] = {"Partidas": 1, "Vitorias": 0}
                         
-                        #somando a um valor já existente. Se a chave não existir, será que considera 0 pelo +=?
-                        champions_played[participant["championName"]]["Gold por Minuto"] += participant["challenges"]["goldPerMinute"]
-                        #dano por minuto
-                        champions_played[participant["championName"]]["Dano por Minuto"] += participant["challenges"]["damagePerMinute"]
-                        #visionScore
-                        champions_played[participant["championName"]]["Placar de Visão"] += participant["visionScore"]
-                        #visionScoreAdvantageLaneOpponent //Algumas partidas mais antigas não tem esse dado!
-                        #champions_played[participant["championName"]]["Vantagem de Visão"] += participant["challenges"]["visionScoreAdvantageLaneOpponent"]
-                        #teamDamagePercentage
-                        champions_played[participant["championName"]]["Porcentagem de Dano do Time"] += participant["challenges"]["teamDamagePercentage"]
-                        #damageTakenOnTeamPercentage
-                        champions_played[participant["championName"]]["Porcentagem de Tankada do time"] += participant["challenges"]["damageTakenOnTeamPercentage"]
-                        #damageDealtToObjectives Dar um jeito de fazer isso em porcentagem do time também, para manter a comparação
-                        champions_played[participant["championName"]]["Dano a Objetivos"] += participant["damageDealtToObjectives"]
-                    else:                    
-                        if participant["win"]:
-                            champions_played[participant["championName"]] = {"Partidas": 1, "Vitorias": 1}
-                        else:
-                            champions_played[participant["championName"]] = {"Partidas": 1, "Vitorias": 0}
-                    
-                        #adicionando a chave "gold por minuto" se não houver
-                        champions_played[participant["championName"]]["Gold por Minuto"] = participant["challenges"]["goldPerMinute"]
-                        #dano por minuto
-                        champions_played[participant["championName"]]["Dano por Minuto"] = participant["challenges"]["damagePerMinute"]
-                        #visionScore
-                        champions_played[participant["championName"]]["Placar de Visão"] = participant["visionScore"]
-                        #visionScoreAdvantageLaneOpponent //Algumas partidas mais antigas não tem esse dado!
-                        #champions_played[participant["championName"]]["Vantagem de Visão"] = participant["challenges"]["visionScoreAdvantageLaneOpponent"]
-                        #teamDamagePercentage
-                        champions_played[participant["championName"]]["Porcentagem de Dano do Time"] = participant["challenges"]["teamDamagePercentage"]
-                        #damageTakenOnTeamPercentage
-                        champions_played[participant["championName"]]["Porcentagem de Tankada do time"] = participant["challenges"]["damageTakenOnTeamPercentage"]
-                        #damageDealtToObjectives Dar um jeito de fazer isso em porcentagem do time também, para manter a comparação
-                        champions_played[participant["championName"]]["Dano a Objetivos"] = participant["damageDealtToObjectives"]
-                        
-                    break
+                            #adicionando a chave "gold por minuto" se não houver
+                            champions_played[participant["championName"]]["Gold por Minuto"] = participant["challenges"]["goldPerMinute"]
+                            #dano por minuto
+                            champions_played[participant["championName"]]["Dano por Minuto"] = participant["challenges"]["damagePerMinute"]
+                            #visionScore
+                            champions_played[participant["championName"]]["Placar de Visão"] = participant["visionScore"]
+                            #visionScoreAdvantageLaneOpponent //Algumas partidas mais antigas não tem esse dado!
+                            #champions_played[participant["championName"]]["Vantagem de Visão"] = participant["challenges"]["visionScoreAdvantageLaneOpponent"]
+                            #teamDamagePercentage
+                            champions_played[participant["championName"]]["Porcentagem de Dano do Time"] = participant["challenges"]["teamDamagePercentage"]
+                            #damageTakenOnTeamPercentage
+                            champions_played[participant["championName"]]["Porcentagem de Tankada do time"] = participant["challenges"]["damageTakenOnTeamPercentage"]
+                            #damageDealtToObjectives Dar um jeito de fazer isso em porcentagem do time também, para manter a comparação
+                            champions_played[participant["championName"]]["Dano a Objetivos"] = participant["damageDealtToObjectives"]
+                            
+                        break
         
     #fazendo o winrate (vitorias/partidas)*100 e outras medias
     for champion, stats in champions_played.items():
@@ -143,6 +160,13 @@ def search_for_champs_played_in_folder_of_matches(player_name):
         champions_played[champion]["Porcentagem de Tankada do time"] = stats["Porcentagem de Tankada do time"]/stats["Partidas"]
         champions_played[champion]["Dano a Objetivos"] = stats["Dano a Objetivos"]/stats["Partidas"]
     
+    #salvando todos os campẽos com que jogou
+    file_name = player_name + "_campeões_jogados.json"
+    champions_played_ordenados = dict(sorted(champions_played.items(), key = lambda x: x[1]["Partidas"], reverse=True))
+    with open(file_name, 'w') as cpo:
+        json.dump(champions_played_ordenados, cpo)
+
+    #criando um dossie com os campeões mais relevantes do player
     medida_de_relevância = 3 #palpite inicial, 4 é mais ou menos 8% das partidas do player, 3 é 6%
     champions_relevantes = {} #devolver apenas campeões com que jogou mais do que a medida de relevância
     for champion in champions_played.keys():
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import player_data_collection as pdc
 import get_match_data as gmd
+import composite_functions as fun
 
 
 primeiro_de_marco_2023 = 1677639600
@@ -13,15 +14,15 @@ byte_b = ["TuniKrime", "Allora", "ASW Frajola", "xXVitoReisXx"]
 #byte_b = []
 cyber_seniors = ["sNowayBr", "Kuhaku", "KÄT CHUP", "h J", "DaviPirado"]
 #cyber_seniors = []
-estrelas = ["Bolianinouli", "Linus501512", "Yo Megumy S2", "Egirl Rolezeira", "GatinhaArisca", "Anonineteen"]
+estrelas = ["BolianiNouli", "Linus501512", "Yo Megumy S2", "Egirl Rolezeira", "GatinhaArisca", "Anonineteen"]
 #estrelas = ["GatinhaArisca", "Anonineteen"]
 grito = ["Quaglietta", "James The 7th", "C0UL0BRE", "ANICIO", "Jose Ze"]
 #grito = []
-o_conselho = ["BruselsPuroMalte", "F1dget Spinner", "ILLEOI", "Kandriel", "Uchisori"]
+o_conselho = ["BruselsPuroMalte", "F1dget Spinner", "ILLEOI", "Kandriel", "Uchisori", "GodKira678", "Mach4jhin"]
 #o_conselho = []
 soca_fofo =  ["Cachorro", "Brunocc31", "WarWickSocaFofo", "CJotaa"]
 #soca_fofo = []
-tropa = ["Lar1nhacs", "OMT Emisu", "Brockadorr", "aninhaclarasc", "A Thresh Talker ", "Nidhogg30"]
+tropa = ["Lar1nhacs", "OMT Emisu", "Brockadorr", "aninhaclarasc", "A Thresh Talker", "Nidhogg30"]
 #tropa = []
 
 times = {
@@ -51,43 +52,15 @@ data_folder = "dados_dos_times_corujão"
 path = os.getcwd()
 data_folder_path = os.path.join(path, data_folder)
 
+# fun.adicionar_ID_partidas_em_csv_para_time("O Conselho", o_conselho, data_folder, primeiro_de_fevereiro_2023)
+
+# fun.atualizar_partidas_de_todos_jogadores(times, data_folder)
+
 for time in times:
-    time_folder = os.path.join(os.path.abspath(data_folder_path), time)
-    os.chdir(time_folder)
+    pasta_do_time = os.path.join(data_folder_path, time)
     for player in times[time]:
-       #print(os.getcwd())
-       #print(os.listdir(os.getcwd()))
-        lista_matches = player + "_matches.csv"
-        player_folder_exists = os.path.exists(player)
-        #os dados de cada partida são salvos em uma pasta com o nome do jogador
-        if player_folder_exists:
-            with open(lista_matches, "r") as file: 
-                contents = [ partida.strip() for partida in file]
-                #print( contents)
-                for partida in contents:
-                    partida_file = partida + ".json"
-                    #print(os.path.join(time_folder,player, partida_file))
-                    #print(os.path.isfile(os.path.join(time_folder,player, partida_file)))
-                    if not os.path.isfile(os.path.join(time_folder,player, partida_file)):
-                        print("Salvando partida {} de {}".format(partida, player))
-                        gmd.store_match_data(gmd.get_all_match_data(partida), player)
-                    else:
-                        print("!!Partida {} já existe!!".format(partida))
-        else:
-            with open(lista_matches, "r") as file:
-                contents = [ partida.strip() for partida in file]
-                #print( contents)
-                #get_all_match_data já cria uma pasta caso ela não exista
-                for partida in contents:
-                    print("Salvando partida {} de {}".format(partida, player))
-                    gmd.store_match_data(gmd.get_all_match_data(partida), player)
-    
-        
-           
-
-            
-#contents agora contem uma lista com os matchs que o player jogou
-#preciso pegar os dados desse match SE não estiver na pasta com o nome do jogador
-
+        print("Criando o dossie de ", player)
+        summoner_infos = gmd.get_summoner_by_name(player)
+        fun.search_for_champs_played_in_folder_of_matches(player, summoner_infos["puuid"], pasta_do_time)
                 
                 
